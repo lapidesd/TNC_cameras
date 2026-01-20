6b6634760d4d1f7a2dc10014a82eb4c1a990fe39
lapidesd <danalapides@gmail.com>
2026-01-20 10:35:34 -0700

    more camera data

---
+++ Code/move_photos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /Volumes/Untitled/DCIM/100DSCIM/
+for file in *; do
+  # Check if it is a file
+  if [ -f "$file" ]; then
+    # Get filename without extension
+    base="${file%.*}"
+    # Get the extension
+    ext="${file##*.}"
+    
+    # Rename
+    # If the file has no extension, just append _1
+    if [ "$base" == "$file" ]; then
+      mv "$file" "/Users/danalapides/Documents/Jupyter/TNC_cameras/Data/${file}_1"
+    else
+      mv "$file" "/Users/danalapides/Documents/Jupyter/TNC_cameras/Data/${base}_1.${ext}"
+    fi
+  fi
+done
+cd ~/Documents/Jupyter/TNC_cameras/Code
